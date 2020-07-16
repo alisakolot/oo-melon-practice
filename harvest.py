3f8a427ff71c61d0ff2e4d3c7bee7cbb27ca58b2
Alisa Kolot <kolot.alisa@gmail.com>
2020-07-16 15:01:52 -0500

    Pairing Info

--- harvest.py
+++ harvest.py
@@ -6,31 +6,72 @@
 class MelonType(object):
     """A species of melon at a melon farm."""
 
-    def __init__(self, code, first_harvest, color, is_seedless, is_bestseller, 
-                 name):
+    def __init__(self, code, first_harvest, color, is_seedless, is_bestseller, name):
         """Initialize a melon."""
+        #Activate the 'variables'/traits ^parameters^ related to each object by telling the function to create a template. 
+        #Follow parameter order.
 
-        self.pairings = []
+        self.code = code
+        self.first_harvest = first_harvest
+        self.color = color
+        self.is_seedles = is_seedless
+        self.is_bestseller = is_bestseller
+        self.name = name
+        
 
         # Fill in the rest
 
     def add_pairing(self, pairing):
         """Add a food pairing to the instance's pairings list."""
+        #Adds pairing information (pairing "method") 
 
-        # Fill in the rest
+        self.add_pairing = pairing
 
     def update_code(self, new_code):
         """Replace the reporting code with the new_code."""
+        #Overriding the previous code used to describe melon traits/. 
 
-        # Fill in the rest
+        self.code = new_code 
 
 
 def make_melon_types():
     """Returns a list of current melon types."""
+    #Specifying the traits/information about the melons. 
+    #Using the call/order 'MelonType' to specify information about the Melon.
 
     all_melon_types = []
 
-    # Fill in the rest
+    # code = MelonType(code, name, first_harvest, color, is_seedless, is_bestseller)
+
+    #Muskmelon:
+    musk = MelonType('musk', 'Muskmelon', 1998, 'green',True, True)
+    musk.add_pairing('mint') #This specifies the pairing 
+    all_melon_types.append(musk) #This gives us the name of the melon
+
+    #Casaba:
+    cas = MelonType("cas", "Casaba", 2003, 'orange', True, False)
+    cas.add_pairing('strawberries, mint')
+    all_melon_types.append(cas)
+    
+    #Crenshaw:
+    cren = MelonType('cren', 'Crenshaw', 1996, 'green', True, False)
+    cren.add_pairing('proscuitto')
+    all_melon_types.append(cren)
+
+    #Yellow Watermelon:
+    yw = MelonType('yw', "Yellow Watermelon", 2013, 'yellow', True, True)
+    yw.add_pairing("ice cream")
+    all_melon_types.append(yw)
+    
+
+
+
+
+    all_melon_types.append(musk)
+
+    print(all_melon_types)
+    
+
 
     return all_melon_types
 
